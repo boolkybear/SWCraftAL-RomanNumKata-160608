3c856a35a80c36151730785dafed77a1b5376c99
Jose Servet <boolkybear@gmail.com>
2016-06-11 17:39:28 +0200

    Pasar tests 1976. En este punto, la kata estaría completa

--- Kata/Int+Roman.swift
+++ Kata/Int+Roman.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 func getIndex(value: Int) -> Int? {
-	let specialValues = [1000, 500, 400, 100, 90, 50, 40, 10, 5]
+	let specialValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 5]
 	
 	for valor in specialValues {
 		if value > valor {
@@ -25,7 +25,7 @@ func fromDictionary(value: Int) -> String {
 	
 	let dictionary: [Int: String] = [	1: "I", 4: "IV", 5: "V", 9: "IX",
 										10: "X", 40: "XL", 50: "L", 90: "XC",
-										100: "C", 400: "CD", 500: "D",
+										100: "C", 400: "CD", 500: "D", 900: "CM",
 										1000: "M"]
 	
 	if let romanValue = dictionary[value] {
